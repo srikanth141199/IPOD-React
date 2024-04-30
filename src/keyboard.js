5687751ea3bfe0176955d195f3cb799643900ffc
Srikanth K <kolleparasrikanth@gmail.com>
2024-04-30 20:49:28 +0530

    keyboard CSS implemented

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,15 +1,29 @@
 import React from "react";
 
-export default class Keyboard extends React.Component{
-
-    render(){
-        return <>
-            <h1>Keyboard</h1>
-            <button onClick={this.props.selectButtonClicked}>SELECT</button>
-            <button onClick={this.props.menuButtonClicked}><i class="fa-solid fa-bars"></i></button>
-            <button onClick={this.props.rightButtonClicked}><i class="fa-solid fa-forward"></i></button>
-            <button onClick={this.props.leftButtonClicked}><i class="fa-solid fa-backward"></i></button>
-            <button onClick={this.props.playPauseButtonClicked}><i class="fa-solid fa-play"></i><i class="fa-solid fa-pause"></i></button>
-        </>
-    }
+export default class Keyboard extends React.Component {
+  render() {
+    return (
+      <div className="keyboard-container">
+        <button onClick={this.props.selectButtonClicked} className="selectBtn">
+          SELECT
+        </button>
+        <button onClick={this.props.menuButtonClicked} className="menuBtn">
+          <i class="fa-solid fa-bars"></i>
+        </button>
+        <button onClick={this.props.rightButtonClicked} className="forwardBtn">
+          <i class="fa-solid fa-forward"></i>
+        </button>
+        <button onClick={this.props.leftButtonClicked} className="backwardBtn">
+          <i class="fa-solid fa-backward"></i>
+        </button>
+        <button
+          onClick={this.props.playPauseButtonClicked}
+          className="playPauseBtn"
+        >
+          <i class="fa-solid fa-play"></i>
+          <i class="fa-solid fa-pause"></i>
+        </button>
+      </div>
+    );
+  }
 }
